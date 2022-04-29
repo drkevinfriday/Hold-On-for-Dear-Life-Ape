2aeed2f51b89d5c1e3210df161917ec82574eae0
Kevin Friday <kevinfriday87@gmail.com>
2022-04-28 22:15:53 -0500

    push test

--- api test/script2.js
+++
@@ -1,9 +0,0 @@
-const finnhub = 'finnhub';
-
-const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-api_key.apiKey = ""
-const finnhubClient = new finnhub.DefaultApi()
-
-finnhubClient.symbolSearch('AAPL','DOW', (error, data, response) => {
-  console.log(data)
-});
